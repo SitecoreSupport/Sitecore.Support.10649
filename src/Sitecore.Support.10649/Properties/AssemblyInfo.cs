97b05b2e5f36a333e82532d443ac5fbb706efcfa
AntonKechashin <atk@sitecore.net>
2017-12-08 13:54:19 +0200

    Added reusable="false" to the SitemapHandler processor

---
+++ src/Sitecore.Support.10649/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.10649")]
+[assembly: AssemblyProduct("Sitecore.Support.10649")]
+[assembly: ComVisible(false)]
